c3914f5a722db5d32ab6501e21c0c3f0bfad4764
Mike Edenfield <kutulu@kutulu.org>
2018-10-09 16:48:07 -0400

    * Use custom Python environment site-extension instead of stock virtual envrionment * Switch to HttpPlatformHandler instead of FastCGI

--- run.py
+++ run.py
@@ -2,9 +2,9 @@ from slackstackbot import app
 
 if __name__ == "__main__":
     import os
-    HOST = os.environ.get("SERVER_HOST", "localhost")
+
     try:
-        PORT = int(os.environ.get("SERVER_PORT", "5555"))
+        PORT = int(os.environ.get("HTTP_PLATFORM_PORT ", "5555"))
     except ValueError:
         PORT = 5555
-    app.run(HOST, PORT)
+    app.run("0.0.0.0", PORT)
